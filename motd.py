cba9c44a8a95eac89488e7bf9163abb887ee5a75
Jay <schu0527@flinders.edu.au>
2023-06-09 15:59:01 +0930

    init

--- motd.py
+++ motd.py
@@ -6,6 +6,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 headings = soup.find_all("a", {"data-testid": "Heading", "class": "text__text__1FZLe text__dark-grey__3Ml43 text__medium__1kbOh text__heading_5_and_half__3YluN heading__base__2T28j heading_5_half title__title__1Sa2a title"})
 
+
 for heading in headings:
     href = heading["href"]
     link = "www.reuters.com"+href
